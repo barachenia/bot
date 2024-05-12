d263e22354aa38ede1abf042d100635399762d7a
Ilya Barachenia <ilya.barachenia@gmail.com>
2024-05-12 16:17:39 +0200

    feat: update handler

--- api/index.js
+++ api/index.js
@@ -1,18 +1,6 @@
 import { startVercel } from '../src/index.js';
-import cors from 'cors';
 
 export default async function handle(req, res) {
-    // Set CORS headers to allow all domains
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        // Handle preflight request for CORS
-        res.status(200).end();
-        return;
-    }
-
     try {
         await startVercel(req, res);
     } catch (e) {
